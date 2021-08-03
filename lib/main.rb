c0e35369392312f40f719d747e3c051e539434bf
jmorton138 <jmorton138@gmail.com>
2021-08-03 09:11:48 +0100

    Insert and delete methods

--- lib/main.rb
+++ lib/main.rb
@@ -34,23 +34,69 @@ class Tree
 
 end
 def insert(root, value)
-    if value < root.data && root.left_child == nil
-        return root.left_child = Node.new(value)
+    return root.left_child = Node.new(value) if value < root.data && root.left_child == nil
+    return root.right_child = Node.new(value) if value > root.data && root.right_child == nil
+    return insert(root.left_child, value) if value < root.data
+    return insert(root.right_child, value) if value > root.data 
+end
+
+def delete(root, value)
+
+    if root.left_child.data == value
+        #first case: deleting leaf in tree
+        if root.left_child.left_child == nil && root.left_child.right_child == nil
+            root.left_child = nil
+            return 
+        # second case: node with one child, always point parent to child
+        elsif root.left_child.right_child != nil && root.left_child.left_child == nil
+             root.left_child = root.left_child.right_child
+             return
+        #3rd case
+        elsif root.left_child.left_child != nil && root.left_child.right_child != nil
+            min_root = root.left_child.right_child
+            min = find_min(min_root)
+            root.left_child.data = min.data
+            delete(root.left_child, min.data)
+            return
+        end
+    elsif root.right_child.data == value
+        #first case
+        if root.right_child.left_child == nil && root.right_child.right_child == nil
+            root.right_child = nil
+            return
+        #second case    
+        elsif root.right_child.right_child != nil && root.right_child.left_child == nil
+            root.right_child = root.right_child.right_child
+            return
+        elsif root.right_child.left_child != nil && root.right_child.right_child != nil
+            min_root = root.right_child.right_child
+            min = find_min(min_root)
+            root.right_child.data = min.data
+            delete(root.right_child, min.data)
+            return
+        end
+            
     end
-    if value > root.data && root.right_child == nil
-        return root.right_child = Node.new(value)
-    end
+
     if value < root.data
-        return insert(root.left_child, value)
+        delete(root.left_child, value)
     end
     if value > root.data
-        return insert(root.right_child, value)
-    end 
+        delete(root.right_child, value)
+    end
+
+end
+
+def find_min(root)
     
+    if root.left_child == nil  
+        return root
+    else
+        find_min(root.left_child) 
+    end
 end
 
 def find(root, value)
-    #search and find value
     return root if root == nil || root.data == value
     return find(root.left_child, value) if root.data > value
     return find(root.right_child, value) if root.data < value
@@ -79,13 +125,22 @@ end
 array = [1, 2, 3, 4, 5, 6, 7]
 #array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-p tree = Tree.new(array)
+tree = Tree.new(array)
 #pretty_print(tree.root)
 #pre_order(tree.root)
-#insert(tree.root, 2)
-
+#insert(tree.root, 1.5)
+pretty_print(tree.root)
+delete(tree.root, 6)
 pretty_print(tree.root)
 
 
-
-
+# #2 child nodes
+# if root.left_child != nil && root.right_child != nil
+#     #find next biggest: min on right (or max on left)
+#     #keep going to the right until no left child, this is value to be replaced with deleted value
+#     delete(root.right_child, value)
+# elsif root.right_child != nil && root.left_child == nil
+#     #this is your min value on right
+#     min = root.right_child
+#     #replace
+# end
